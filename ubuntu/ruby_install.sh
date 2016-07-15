386a2bd3554aadfeab5098b4d805ac200e36f758
Jacob Nollette <jaco@jacobnollette.com>
2016-07-15 16:30:18 -0400

    updated ruby install, to not include shell reup

--- ubuntu/ruby_install.sh
+++ ubuntu/ruby_install.sh
@@ -1,4 +1,4 @@
-cd ;
+#cd ;
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv;
 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc;
@@ -7,16 +7,23 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc;
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc;
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc;
 
-exec $SHELL;
+#echo $SHELL;
+#exec $SHELL;
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build;
 
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc;
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zshrc;
 
-exec $SHELL;
+#exec $SHELL;
 
 rbenv install 2.3.1;
 rbenv global 2.3.1;
 
 ruby -v;
+
+#gem install bundler;
+#gem install capistrano;
+#gem install rails -v 4.2.6;
+
+#rbenv rehash;
