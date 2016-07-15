89f69b8e3fcef8479ad3d5ddd7d4a6c8522df3dd
Jacob Nollette <jacob@jacobnollette.com>
2016-07-15 17:56:19 -0400

    added ruby deploy script that works

---
+++ ubuntu/14_04/ruby/install.sh
@@ -0,0 +1,29 @@
+
+# get the working directory
+PWD=$(pwd;);
+#echo $PWD;
+
+#cd ;
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv;
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build;
+
+cat $PWD/partials/bashrc >> ~/.bashrc;
+cat $PWD/partials/bashrc >> ~/.zshrc;
+
+#source $HOME/.zshrc;
+
+
+exec $PWD/partials/ruby_install.sh;
+#echo $SHELL;
+#exec $SHELL;
+
+#rbenv install 2.3.1;
+#rbenv global 2.3.1;
+
+#ruby -v;
+
+#gem install bundler;
+#gem install capistrano;
+#gem install rails -v 4.2.6;
+
+#rbenv rehash;
---
+++ ubuntu/14_04/ruby/partials/ruby_install.sh
@@ -0,0 +1,11 @@
+rbenv install 2.3.1;
+rbenv global 2.3.1;
+
+ruby -v;
+
+gem install bundler;
+gem install capistrano;
+gem install rails -v 4.2.6;
+gem install compass;
+
+rbenv rehash;
