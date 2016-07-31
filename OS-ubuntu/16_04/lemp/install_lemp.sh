2a27df374ff535b68ef9353963967cc657e73c7f
Jacob Nollette <jacob@jacobnollette.com>
2016-07-30 23:52:47 -0500

    added lemp 16_04

--- OS-ubuntu/16_04/lemp/install_lemp.sh
+++ OS-ubuntu/16_04/lemp/install_lemp.sh
@@ -1,2 +1,6 @@
 sudo apt-get update; sudo apt-get upgrade;
-sudo apt-get install nginx mysql-server php5-fpm php5 php5-mcrypt php5-cli php5-curl php5-dev php5-gd php5-gmp php5-mysql php5-sqlite php5-pgsql tmux zip unzip wget;
+sudo apt-get isntall tmux zip unzip wget;
+
+sudo apt-get install nginx php-fpm php-mysql
+
+#sudo apt-get install nginx mysql-server php5-fpm php5 php5-mcrypt php5-cli php5-curl php5-dev php5-gd php5-gmp php5-mysql php5-sqlite php5-pgsql tmux zip unzip wget;
