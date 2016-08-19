9e5e8607fa1a2948ecb69a9c11342203667755a0
Jacob Nollette <jacob@jacobnollette.com>
2016-08-19 19:11:35 -0400

    lemp 7 install 16.04

--- OS-ubuntu/16_04/lemp/install_lemp-7.sh
+++ OS-ubuntu/16_04/lemp/install_lemp-7.sh
@@ -1,6 +1,13 @@
 sudo apt-get update; sudo apt-get upgrade;
-sudo apt-get isntall tmux zip unzip wget;
+#sudo apt-get isntall tmux zip unzip wget;
 
-sudo apt-get install nginx php-fpm php-mysql
+sudo apt-get install \
+	nginx \
+	php7.0-fpm php7.0-cli php7.0-dev \
+	php7.0-mysql php7.0-sqlite php7.0-pgsql \
+	php7.0-mcrypt \
+	php7.0-curl \
+	php7.0-gd php7.0-gmp \
+	php7.0-cli php7.0-curl ;
 
 #sudo apt-get install nginx mysql-server php5-fpm php5 php5-mcrypt php5-cli php5-curl php5-dev php5-gd php5-gmp php5-mysql php5-sqlite php5-pgsql tmux zip unzip wget;
