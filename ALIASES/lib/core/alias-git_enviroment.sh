86cb59704602232ca9f9bc8a5ae61ef115a5f66c
Jacob Nollette <jacob@jacobnollette.com>
2017-08-30 15:50:21 -0500

    updated aliases

--- ALIASES/lib/core/alias-git_enviroment.sh
+++ ALIASES/lib/core/alias-git_enviroment.sh
@@ -4,3 +4,15 @@ function env_push () {
 	git push staff $1;
 }
 
+function tower_push () {
+	git push tower $1;
+}
+
+function github_push () {
+	git push github $1;
+}
+
+function nas_push () {
+	git push project $1;
+	git push staff $1;
+}
