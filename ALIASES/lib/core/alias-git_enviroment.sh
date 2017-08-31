34ab2cc943f4bd6d97f947b1f8a06bb7b1d142ae
Jacob Nollette <jacob@jacobnollette.com>
2017-08-31 12:10:16 -0500

    more git enviroment stuff

--- ALIASES/lib/core/alias-git_enviroment.sh
+++ ALIASES/lib/core/alias-git_enviroment.sh
@@ -8,11 +8,35 @@ function tower_push () {
 	git push tower $1;
 }
 
+function tower_pull () {
+	git pull tower $1;
+}
+
 function github_push () {
 	git push github $1;
 }
 
-function nas_push () {
+function github_pull () {
+	git pull github $1;
+}
+
+function project_push () {
 	git push project $1;
+}
+
+function project_pull () {
+	git pull project $1;
+}
+
+function staff_push () {
 	git push staff $1;
+}
+
+function staff_pull () {
+	git pull staff $1;
+}
+
+function nas_push () {
+	staff_push $1;
+	project_push $1;
 }
