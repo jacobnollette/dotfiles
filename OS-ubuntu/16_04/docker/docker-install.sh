7a4b7544e457f10ed606bdd2ac6b2c4aedfb0637
Jacob Nollette <jacob@jacobnollette.com>
2017-09-24 06:27:16 +0000

    updates to docker stuff on 16.04

--- OS-ubuntu/16_04/docker/docker-install.sh
+++ OS-ubuntu/16_04/docker/docker-install.sh
@@ -4,6 +4,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update;
 apt-cache policy docker-ce;
 sudo apt-get install -y docker-ce;
-sudo systemctl status docker;
+#sudo systemctl status docker;
 
 
