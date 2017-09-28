d6b57985edc239a6700c872ee5ca26744d0856d0
Jacob Nollette <jacob@jacobnollette.com>
2017-09-28 15:22:24 +0000

    more tops

--- systems/ffmpeg_build.sh
+++ systems/ffmpeg_build.sh
@@ -1,1 +1,2 @@
-apt-get install -y ffmpeg tmux zip wget openvpn zsh cifs-utils tmux vim
+apt-get install -y ffmpeg tmux zip wget openvpn zsh cifs-utils tmux vim;
+apt-get install -y iftop iotop htop;
