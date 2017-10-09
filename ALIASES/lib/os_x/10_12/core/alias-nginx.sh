661918403c85ff0a8c1af80e1a23a4c988694058
Jacob Nollette <jacob@jacobnollette.com>
2017-10-09 13:03:59 -0500

    nginx rebuild, fixed typo

--- ALIASES/lib/os_x/10_12/core/alias-nginx.sh
+++ ALIASES/lib/os_x/10_12/core/alias-nginx.sh
@@ -3,5 +3,5 @@
 
 #		nginx restart
 alias nginx_start='sudo nginx;';
-alias nginx_stop='sudo nginx -f stop;';
+alias nginx_stop='sudo nginx -s stop;';
 alias nginx_restart='nginx_stop; nginx_start;';
