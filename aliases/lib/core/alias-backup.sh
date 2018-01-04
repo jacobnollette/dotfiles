be2b620e8be33870dbba68995cdaa517718797ce
Jacob Nollette <jacob@jacobnollette.com>
2018-01-03 22:53:40 -0600

    alias find loop

--- aliases/lib/core/alias-backup.sh
+++ aliases/lib/core/alias-backup.sh
@@ -4,13 +4,13 @@ function archive_folder () {
 	print_folder="$given_folder/_print";
 	mkdir -p $print_folder;
 
-	for filename in $given_folder/*.zip; do
+	for filename in `find . -name "*.zip" -type f`; do
 		echo $filename
 		gpg --armor -r "jacob@jacobnollette.com" -e $filename;
 		mv "$filename.asc" $print_folder;
 	done;
 
-	for filename in $given_folder/*.tar.gz; do
+	for filename in `find . -name "*.tar.gz" -type f`; do
 		echo $filename
 		gpg --armor -r "jacob@jacobnollette.com" -e $filename;
 		mv "$filename.asc" $print_folder;
