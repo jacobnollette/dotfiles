f92953ace4d91d0e4dc58669a5ebd2723ebc8d05
Jacob Nollette <jacob@jacobnollette.com>
2018-01-11 22:15:22 -0600

    encrypt folder

--- aliases/lib/core/alias-backup.sh
+++ aliases/lib/core/alias-backup.sh
@@ -6,13 +6,13 @@ function archive_folder () {
 
 	for filename in `find $given_folder -name "*.zip" -type f`; do
 		echo $filename
-		gpg --armor -r "jacob@jacobnollette.com" -e $filename;
+		gpg --armor -r "jacob@jacobnollette.com" -e "$filename";
 		mv "$filename.asc" $print_folder;
 	done;
 
 	for filename in `find $given_folder -name "*.tar.gz" -type f`; do
 		echo $filename
-		gpg --armor -r "jacob@jacobnollette.com" -e $filename;
+		gpg --armor -r "jacob@jacobnollette.com" -e "$filename";
 		mv "$filename.asc" $print_folder;
 	done;
 
@@ -33,4 +33,25 @@ function encrypt_file () {
 	given_file="$@";
 	gpg --armor -r "jacob@jacobnollette.com" -e "$given_file";
 	rm -rf $given_file;
+}
+
+
+function encrypt_folder () {
+	
+	given_folder="$@";
+	#print_folder="$given_folder/_print";
+	#mkdir -p $print_folder;
+
+	for filename in `find $given_folder -name "*.zip" -type f`; do
+		echo $filename
+		gpg --armor -r "jacob@jacobnollette.com" -e "$filename";
+		rm "$filename";
+	done;
+
+	for filename in `find $given_folder -name "*.tar.gz" -type f`; do
+		echo $filename
+		gpg --armor -r "jacob@jacobnollette.com" -e "$filename";
+		rm "$filename";
+	done;
+
 }
