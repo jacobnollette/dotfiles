c325eff1c2ac25dc9a0afc7c1266f2689a6b4bd1
Jacob Nollette <jacob@jacobnollette.com>
2018-01-18 13:35:02 -0600

    archive rebuilt

--- aliases/lib/core/alias-backup.sh
+++ aliases/lib/core/alias-backup.sh
@@ -1,4 +1,4 @@
-function archive_folder () {
+function _archive_folder () {
 	
 	given_folder="$@";
 	print_folder="$given_folder/_print";
@@ -18,16 +18,33 @@ function archive_folder () {
 
 }
 
-function archive_file () {
 
+tar_zip () {
+	    tar -zcvf $1 $2; 
+}
 
-	given_file="$@";
-	mkdir -p _print;
+tar_unzip () {
+	  tar -xvf $1; 
+}
+
+function archive_folder () {
+
+	given_folder="$@";
 	
-	gpg --armor -r "jacob@jacobnollette.com" -e "$given_file";
-	mv "$given_file.asc" _print;
+	given_start=`pwd`;
+
+	cd "$given_folder";
+	CURRENT=`pwd`
+	given_base=`basename "$CURRENT"`
+	cd "$given_start";
+	given_date=`date '+%Y-%m-%d-%H-%M-%S'`;
+	given_file="$given_base-$given_date.tar.gz";
+
+	tar -zcvf "$given_file" "$given_folder";
+
 
 }
+
 
 function encrypt_file () {
 	given_file="$@";
@@ -36,22 +53,34 @@ function encrypt_file () {
 }
 
 
+function encrypt_file_save () {
+	given_file="$@";
+	gpg --armor -r "jacob@jacobnollette.com" -e "$given_file";
+	#rm -rf $given_file;
+}
+
+
 function encrypt_folder () {
 	
 	given_folder="$@";
-	#print_folder="$given_folder/_print";
-	#mkdir -p $print_folder;
 
-	for filename in `find $given_folder -name "*.zip" -type f`; do
+	for filename in $given_folder; do
 		echo $filename
 		gpg --armor -r "jacob@jacobnollette.com" -e "$filename";
 		rm "$filename";
 	done;
 
-	for filename in `find $given_folder -name "*.tar.gz" -type f`; do
+}
+
+
+function encrypt_folder_save () {
+	
+	given_folder="$@";
+
+	for filename in $given_folder; do
 		echo $filename
 		gpg --armor -r "jacob@jacobnollette.com" -e "$filename";
-		rm "$filename";
+		#rm "$filename";
 	done;
 
 }
